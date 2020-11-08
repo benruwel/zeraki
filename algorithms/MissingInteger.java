088c9209d71726f0eeef3b73fd4d50e61aa58d8b
Ben Ruwel <benruwel21@gmail.com>
2020-11-08 09:40:24 +0300

    Use local variable correctly

--- algorithms/MissingInteger.java
+++ algorithms/MissingInteger.java
@@ -3,18 +3,18 @@ import java.util.Arrays;
 public class MissingInteger {
 
     static int findMissingInteger(int[] K){
-        int missingNum;
+        int missingNum = 0;
         int n = K.length;
         //sort the array in ascending order
         Arrays.sort(K);
         for(int i = 0; i < n; i++) {
             int expectedNextNum = K[i] + 1;
             if(i < n-1 && expectedNextNum != K[i+1]){
-                return expectedNextNum;
+                missingNum = expectedNextNum;
             }
         }
-        //return 0 if there is no missing integer
-        return 0;
+        //returns 0 if there is no missing integer
+        return missingNum;
     }
 
     public static void main(String[] args) {
