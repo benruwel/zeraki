55eb70935ed7afe605d866a3daaacc2d538197e4
Ben Ruwel <benruwel21@gmail.com>
2020-11-08 17:17:59 +0300

    Complete sql query to create the tables

---
+++ sql/create.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE education;
+\c education;
+
+CREATE TABLE student(
+    studentid int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(250),
+    course int(11),
+    PRIMARY KEY(studentid),
+    FOREIGN KEY(course) REFERENCES course(courseid)
+);
+
+CREATE TABLE IF NOT EXISTS course(
+    courseid int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(250),
+    institution int(11),
+    PRIMARY KEY(courseid),
+    FOREIGN KEY(institution) REFERENCES institution(institutionid)
+);
+
+CREATE TABLE institution(
+    institutionid int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(250),
+    PRIMARY KEY(institutionid)
+);
